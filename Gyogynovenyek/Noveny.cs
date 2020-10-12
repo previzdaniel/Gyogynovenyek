c9421fd240939f151ea42950fdc523a04c73b044
previzdaniel <dani.2000.2x@gmail.com>
2020-10-12 12:55:55 +0200

    Add project files.

---
+++ Gyogynovenyek/Noveny.cs
@@ -0,0 +1,29 @@
+﻿namespace Gyogynovenyek
+{
+    internal class Noveny
+    {
+        public string Nev { get; private set; }
+        public string Resz { get; private set; }
+        public int Kezd { get; private set; }
+        public int Veg { get; private set; }
+        public int Idotartam { get; private set; }
+
+        public Noveny(string szoveg)
+        {
+            string[] sor = szoveg.Split(';');
+            Nev = sor[0];
+            Resz = sor[1];
+            Kezd = int.Parse(sor[2]);
+            Veg = int.Parse(sor[3]);
+
+            if (Veg > Kezd)
+            {
+                Idotartam = (Veg - Kezd) + 1;
+            }
+            else
+            {
+                Idotartam = (12 - Kezd) + Veg + 1;
+            }
+        }
+    }
+}
---
+++ Gyogynovenyek/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Gyogynovenyek
+{
+    class Program
+    {
+        static List<Noveny> novenyek = new List<Noveny>();
+        static Dictionary<string, int> reszek = new Dictionary<string, int>();
+
+        static void Beolvas()
+        {
+            StreamReader file = new StreamReader("noveny.csv");
+            while (!file.EndOfStream)
+            {
+                novenyek.Add(new Noveny(file.ReadLine()));
+            }
+            file.Close();
+        }
+
+        static void feladat1()
+        {
+            Console.WriteLine("Növények száma: {0}",novenyek.Count());
+        }
+
+        static void feladat2()
+        {
+            Console.WriteLine("\n2. feladat: gyűjthető részek.");
+            foreach (var n in novenyek)
+            {
+                if (!reszek.ContainsKey(n.Resz))
+                {
+                    reszek.Add(n.Resz, 0);
+                }
+            }
+
+            foreach (var n in novenyek)
+            {
+                reszek[n.Resz]++;
+            }
+
+            foreach (var r in reszek)
+            {
+                Console.WriteLine(r.Key + ";" + r.Value);
+            }
+        }
+
+        static void feladat3()
+        {
+            int max = 0;
+            foreach (var n in novenyek)
+            {
+                if (n.Idotartam > max)
+                {
+                    max = n.Idotartam;
+                }
+            }
+
+            foreach (var n in novenyek)
+            {
+                if (max == n.Idotartam)
+                {
+                    Console.WriteLine("\nA legtöbb idő amíg gyűjthető: {0}, {1}",max, n.Nev);
+                }
+            }
+        }
+
+        static void feladat4()
+        {
+            double db = 0;
+            int osszeg = 0;
+            foreach (var n in novenyek)
+            {
+                osszeg += n.Idotartam;
+                db++;
+            }
+            Console.WriteLine("Átlagos gyűjthető időtartam: {0}", osszeg / db);
+        }
+
+        static void Main(string[] args)
+        {
+            Beolvas();
+            feladat1();
+            feladat2();
+            feladat3();
+            feladat4();
+
+            Console.ReadKey();
+        }
+    }
+}
